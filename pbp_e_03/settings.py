6bc527fd61d009904e8d4267a20de9abb6595af9
DJaegerScript <adjiedjaka0902@gmail.com>
2022-10-11 08:20:11 +0700

    fixed staticfiles config

--- pbp_e_03/settings.py
+++ pbp_e_03/settings.py
@@ -117,6 +117,18 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+for directory in [*STATICFILES_DIRS, STATIC_ROOT]:
+    directory.mkdir(exist_ok=True)
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
