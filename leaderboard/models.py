dcf9deca5e8244139e51b10ffcb68c37ceb2f41e
DJaegerScript <adjiedjaka0902@gmail.com>
2022-10-27 13:48:30 +0700

    created model for leaderboard & market

--- leaderboard/models.py
+++ leaderboard/models.py
@@ -2,9 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Task(models.Model):
-    date = models.DateField()
-    title = models.CharField(max_length=100)
-    description = models.TextField()
+    donation_amount = models.IntegerField()
+    country = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete =models.CASCADE)
 
 # Create your models here.
--- market/models.py
+++ market/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Product(models.Model):
+    image_url = models.CharField()
+    name = models.CharField()
+    stock = models.IntegerField()
+    price = models.IntegerField()
+
+class TransactionHistory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    date = models.DateField()
